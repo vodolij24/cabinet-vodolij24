8f27de27e7adefaf42262aafac561416d2b46812
YuginProgramming <dudaryev@gmail.com>
2025-04-27 19:21:04 +0300

    upd27and3

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,14 +1,15 @@
 import { CreditCard, DollarSign, Package } from "lucide-react";
 
 import { Separator } from "@/components/ui/separator";
-import { Overview } from "@/components/overview";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
-import { getTotalRevenue } from "@/actions/get-total-revenue";
-import { getSalesCount } from "@/actions/get-sales-count";
-import { getGraphRevenue } from "@/actions/get-graph-revenue";
-import { getStockCount } from "@/actions/get-stock-count";
-import { formatter } from "@/lib/utils";
+
+// import { getTotalRevenue } from "@/actions/get-total-revenue";
+// import { getSalesCount } from "@/actions/get-sales-count";
+// import { getGraphRevenue } from "@/actions/get-graph-revenue";
+// import { getStockCount } from "@/actions/get-stock-count";
+// import { formatter } from "@/lib/utils";
+// import { Overview } from "@/components/overview";
 
 interface DashboardPageProps {
   params: {
@@ -16,9 +17,7 @@ interface DashboardPageProps {
   };
 };
 
-const DashboardPage: React.FC<DashboardPageProps> = async ({ 
-  params
-}) => {
+const DashboardPage: React.FC<DashboardPageProps> = async () => {
   //const totalRevenue = await getTotalRevenue(params.storeId);
   //const graphRevenue = await getGraphRevenue(params.storeId);
   //const salesCount = await getSalesCount(params.storeId);
