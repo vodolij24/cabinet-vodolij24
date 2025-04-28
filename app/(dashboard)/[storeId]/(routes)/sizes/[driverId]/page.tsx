a6533a611a9f507eb53c0e8a72154276eeec6a94
YuginProgramming <dudaryev@gmail.com>
2025-04-28 16:57:03 +0300

    upd28and3

--- app/(dashboard)/[storeId]/(routes)/sizes/[driverId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/[driverId]/page.tsx
@@ -1,20 +1,22 @@
+
 import prismadb from "@/lib/prismadb";
 import { DriversForm } from "./components/driver-form";
 
-interface SizePageProps {
+type SizePageProps = {
   params: {
+    storeId: string; // ← you actually have storeId too in your route
     driverId: string;
   };
-}
+};
 
 const SizePage = async ({ params }: SizePageProps) => {
   const drivers = await prismadb.drivers.findUnique({
     where: {
-      id: params.driverId === 'new' ? 0 : Number(params.driverId)
-    }
+      id: params.driverId === "new" ? 0 : Number(params.driverId),
+    },
   });
 
-  return ( 
+  return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <DriversForm initialData={drivers} />
