cfb70ec1adc5b1f4bb3f2534ea86ea6241480ad7
brysommer <brysommer1@gmail.com>
2026-04-14 14:26:02 +0300

    70 rows per transaction page

--- app/(dashboard)/[storeId]/(routes)/transactions/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/transactions/components/client.tsx
@@ -8,27 +8,27 @@ import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
-import { BotTrransactionsColumn, columns} from "./columns";
+import { BotTrransactionsColumn, columns } from "./columns";
 import { DataTableTransactions } from "@/components/ui/data-table-transactions";
 
 interface SizesClientProps {
   data: BotTrransactionsColumn[];
 }
 
-export const BotTransactions: React.FC<SizesClientProps> = ({
-  data
-}) => {
+export const BotTransactions: React.FC<SizesClientProps> = ({ data }) => {
   const params = useParams();
   const router = useRouter();
 
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title={`Транзакції (${data.length})`} description="Транзакції через бота" />
-      
+        <Heading
+          title={`Транзакції (${data.length})`}
+          description="Транзакції через бота"
+        />
       </div>
       <Separator />
-      <DataTableTransactions searchKey="date" columns={columns} data={data} />
+      <DataTableTransactions searchKey="cardId" columns={columns} data={data} />
     </>
   );
 };
